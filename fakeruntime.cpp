bc97261fddddea4da46ad19df76eca27d858c6e5
Paul Carpenter <paul.carpenter@bsc.es>
2022-04-09 20:56:01 +0200

    Updates to "fakeruntime"

--- fakeruntime.cpp
+++ fakeruntime.cpp
@@ -9,74 +9,100 @@
 #include <cassert>
 #include <errno.h>
 
+#define MAX_NODES 16
 #define TOTAL_CORES_PER_NODE 48
+
+int numNodes = 0;
+int numAppranks = 0;
+
+double loadPerCore[MAX_NODES];
 
 class NanosInstance
 {
     private:
-        int _instanceNum;
+        int _apprankNum;
         int _totalLoad;
-        int _numRanks;
+        int _numIranks;
         pthread_t _thread;
-        std::vector<int> _nodes; // for each i, which node is rank i on?
+        std::vector<int> _extranks; // for each i, which node is rank i on?
+		std::vector<int> _nodes;
         std::vector<int> _cores; // for each i, how many cores does rank i have?
 
     public:
 
-        NanosInstance(int instanceNum, int totalLoad, int numRanks, ...);
+        NanosInstance(int apprankNum, int totalLoad, int numIranks, ...);
 
         void thread(void)
         {
-            std::cout << "Thread for " << _instanceNum << "\n";
-            for (int i=0; i<_numRanks; i++)
+			char filename[1024];
+            std::cout << "Thread for " << _apprankNum << "\n";
+			std::ofstream outfile[MAX_NODES];
+			std::ifstream allocfile;
+			for (int rank=0; rank<_numIranks; rank++) {
+				sprintf(filename, ".hybrid/utilization%d", _extranks[rank]);
+				outfile[rank].open(filename);
+			}
+
+            for (int i=0; i<_numIranks; i++)
             {
-                std::cout << " " << _instanceNum << " " << _nodes[i] << "\n";
+                std::cout << " " << _apprankNum << " " << _extranks[i] << "\n";
             }
 
+			double timestamp = 0.0;
             for(int iter = 0; iter < 1000; iter++)
             {
+				timestamp += 0.5;
                 // Calculate total number of cores
                 int totalCores = 0;
-                for (int i=0; i<_numRanks; i++)
+                for (int i=0; i<_numIranks; i++)
                 {
                     totalCores += _cores[i];
                 }
-                std::cout << _instanceNum << ": iter=" << iter << ", cores=" << totalCores << "\n";
+				loadPerCore[_apprankNum] = (float)_totalLoad / totalCores;
+				std::cout << "loadPerCore " << _apprankNum << " is " << loadPerCore[_apprankNum] << "\n";
+				double maxLoadPerCore = 0;
+				for (int j=0; j< numAppranks; j++) {
+					if (loadPerCore[j] > maxLoadPerCore) {
+						maxLoadPerCore = loadPerCore[j];
+					}
+				}
+				double fracBusy = loadPerCore[_apprankNum] / maxLoadPerCore;
+                std::cout << _apprankNum << ": iter=" << iter << ", cores=" << totalCores << "\n";
 
                 // Print theoretical load
-                for (int rank=0; rank<_numRanks; rank++)
+                for (int rank=0; rank<_numIranks; rank++)
                 {
                     double nodeLoad = (double)_totalLoad * _cores[rank] / totalCores;
-                    char filename[1024];
-                    sprintf(filename, ".balance/load-%d-%d", _instanceNum, _nodes[rank]);
-                    std::ofstream myfile;
-                    myfile.open(filename);
-                    myfile << rank << " " << _cores[rank] << " " << nodeLoad << "\n";
-                    myfile.close();
+					double busyCores = _cores[rank] * fracBusy;
+                    outfile[rank] << timestamp << " " << _cores[rank] << " " << _cores[rank] << " " << busyCores << "\n";
+					outfile[rank].flush();
                 }
 
                 // Get the allocation of cores
                 char filename[1024];
-                sprintf(filename, ".balance/alloc-%d", _instanceNum);
-                std::ifstream myfile;
-                myfile.open(filename);
-                if (myfile.is_open())
+                sprintf(filename, ".hybrid/alloc%d", _apprankNum);
+                allocfile.open(filename);
+                if (allocfile.is_open())
                 {
-                    for (int rank=0; rank<_numRanks; rank++)
+                    for (int rank=0; rank<_numIranks; rank++)
                     {
                         int nodeAlloc;
-                        myfile >> nodeAlloc;
+                        allocfile >> nodeAlloc;
                         if (nodeAlloc != _cores[rank])
                         {
-                            std::cout << "new allocation of " << nodeAlloc << " cores for instance " << _instanceNum << " rank " << rank << "\n";
+                            std::cout << "new allocation of " << nodeAlloc << " cores for instance " << _apprankNum << " rank " << rank << "\n";
                             _cores[rank] = nodeAlloc;
                         }
                     }
-                    myfile.close();
+                    allocfile.close();
                 }
 
-                sleep(1);
+                usleep(500000);
             }
+			for (int rank=0; rank<_numIranks; rank++)
+			{
+				outfile[rank].close();
+			}
         }
 
         void wait(void)
@@ -99,9 +125,9 @@ static void *instance_thread(void *arg)
 }
 
 
-NanosInstance::NanosInstance(int instanceNum, int totalLoad, int numRanks, ...) 
-                            : _instanceNum(instanceNum), _totalLoad(totalLoad), 
-                              _numRanks(numRanks), _nodes(numRanks), _cores(numRanks)
+NanosInstance::NanosInstance(int apprankNum, int totalLoad, int numIranks, ...) 
+                            : _apprankNum(apprankNum), _totalLoad(totalLoad), 
+                              _numIranks(numIranks), _extranks(numIranks), _cores(numIranks)
 {
     int rc;
 
@@ -110,20 +136,43 @@ NanosInstance::NanosInstance(int instanceNum, int totalLoad, int numRanks, ...)
     char buffer[1024];
     gethostname(hostname, 1024);
 
-    va_start(args, numRanks);
-
-    // Reset allocation for the instance
-    char filename[1024];
-    sprintf(filename, ".balance/alloc-%d", _instanceNum);
-    std::ofstream myfile;
-    myfile.open(filename);
-    for (int i=0; i<numRanks; i++)
+	// Set up extrank
+    va_start(args, numIranks);
+	_extranks.resize(numIranks);
+	_nodes.resize(numIranks);
+    for (int i=0; i<numIranks; i++)
     {
-        _nodes[i] = va_arg(args, int);
-        myfile << ((i==0)?48:0) << "\n";
+        _extranks[i] = va_arg(args, int);
+		_nodes[i] = _extranks[i] % numNodes;
+		std::cout << "Apprank " << apprankNum << " irank " << i << " has extRank " << _extranks[i] << " on node " << _nodes[i] << "\n";
         _cores[i] = 0;
     }
+
+    char filename[1024];
+	// Create map files for the instance
+    for (int i=0; i<numIranks; i++) {
+		sprintf(filename, ".hybrid/map%d", _extranks[i]);
+		std::ofstream myfile;
+		myfile.open(filename);
+		myfile << "externalRank " << _extranks[i] << "\n";
+		myfile << "apprankNum " << apprankNum << "\n";
+		myfile << "internalRank " << i << "\n";
+		myfile << "nodeNum " << _nodes[i] << "\n";
+		myfile << "indexThisNode " << _extranks[i] / numNodes << "\n";
+		myfile << "cpusOnNode " << TOTAL_CORES_PER_NODE << "\n";
+		myfile.close();
+	}
+
+    // Reset allocation for the instance
+    sprintf(filename, ".hybrid/alloc%d", _apprankNum);
+    std::ofstream myfile;
+    myfile.open(filename);
+    for (int i=0; i<numIranks; i++)
+    {
+        myfile << ((i==0)?48:1) << "\n";
+    }
     myfile.close();
+
     _cores[0] = TOTAL_CORES_PER_NODE;
     va_end(args);
 
@@ -140,8 +189,8 @@ NanosInstance::NanosInstance(int instanceNum, int totalLoad, int numRanks, ...)
 
 int main(void)
 {
-    system("rm -rf .balance/");
-    system("mkdir -p .balance/");
+    system("rm -rf .hybrid/");
+    system("mkdir -p .hybrid/");
 
     // Map mpi rank to node number
     std::vector<int>map {0,1,2,3, 0,1,2,3};
@@ -154,7 +203,11 @@ int main(void)
     myfile << "\n";
     myfile.close();
 
-    //                 instanceNum  totalLoad, numRanks   mpi ranks
+	numNodes = 4;
+	numAppranks = 4;
+	assert(numNodes <= MAX_NODES);
+
+    //                 apprankNum  totalLoad, numIranks   mpi ranks
     NanosInstance ins0(          0,        10,       2,  0,5);
     NanosInstance ins1(          1,        20,       2,  1,6);
     NanosInstance ins2(          2,         5,       2,  2,7);
